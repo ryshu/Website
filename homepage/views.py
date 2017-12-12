9e75156f62554c4e2c202babc0ef2a04f83eafd8
Oscar MARIE--TAILLEFER <o5mariet@enib.fr>
2017-12-12 11:26:51 +0100

    Change for highlight in asso and news

--- homepage/views.py
+++ homepage/views.py
@@ -1,7 +1,16 @@
 from django.shortcuts import render, redirect, get_object_or_404
-from . import forms
-from . import models
+
+from asso.models import Asso as AssoModel
+from news.models import News as NewsModel
+
 import json
+import operator
 
 def index(request):
-	return render(request, "homepage/index.html")
+
+	context = {
+        'assos' : AssoModel.objects.filter(homepage_highlight = True).order_by('priority').all(),
+        'news' : NewsModel.objects.filter(homepage_highlight = True ).order_by('homepage_highlight')[:3]
+    }
+	return render(request, "homepage/index.html", context)
+
